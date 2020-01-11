91cd116debe1d9e4625df78c057f7a4cd439ea37
howdy-mj <hi.minjungkim@gmail.com>
2020-01-11 21:42:29 +0900

    Changing Color

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 // js내에 canvas pixel modifier에 사이즈를 줘야 출력될 수 있음
 canvas.width = 700;
@@ -31,11 +32,12 @@ function onMouseMove(event){
     }
 }
 
-// 캔버스 위 마우스 클릭시 painting 시작
-function onMouseDown(event){
-    painting = true;
-}
 
+// 클릭한 곳 style.backgroundColor 가져오기
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color; // 선택한 색상이 유지되도록 color에 뒤집어 씌우기
+}
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
@@ -43,3 +45,8 @@ if(canvas){
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+// colors의 색상 위치 가져오기
+Array.from(colors).forEach(color => 
+    color.addEventListener("click", handleColorClick)
+);
