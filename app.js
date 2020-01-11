93e8c00818219cb22ce48532c9ed13ba91a4213c
howdy-mj <hi.minjungkim@gmail.com>
2020-01-11 21:29:57 +0900

    explanation

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
+// js내에 canvas pixel modifier에 사이즈를 줘야 출력될 수 있음
+canvas.width = 700;
+canvas.height = 700;
+
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
@@ -19,11 +23,11 @@ function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
     if(!painting){
-        ctx.beginPath();
+        ctx.beginPath(); // creating path(line)
         ctx.moveTo(x,y);
     } else {
-        ctx.lineTo(x, y); 
-        ctx.stroke();
+        ctx.lineTo(x, y); // path의 이전위치부터 지금까지 line을 잇는 것
+        ctx.stroke(); // 획을 긋는 것
     }
 }
 
